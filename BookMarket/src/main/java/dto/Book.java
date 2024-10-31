c93f88c6a6d0561634313c0ea18db215ca0fece9
Joonhyuk Ahn <96415800+dkswnsgur@users.noreply.github.com>
2024-10-31 21:32:39 +0900

    Add files via upload

--- BookMarket/src/main/java/dto/Book.java
+++ BookMarket/src/main/java/dto/Book.java
@@ -17,7 +17,9 @@ public class Book implements Serializable {
 	private String releaseDate;   //출판일(월/년)
 	private String condition; 		//신제품 or 구제품 or 리퍼브제품
 	private String filename;        //이미지
-
+	private int quantity;			//장바구니에 담은 개수
+	
+	
 	public Book() {
 		super();
 }
@@ -117,5 +119,12 @@ public class Book implements Serializable {
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
 
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 }
