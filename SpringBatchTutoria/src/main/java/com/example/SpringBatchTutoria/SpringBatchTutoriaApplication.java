657c1a045a721c5f55d3e8729a327f92312badec
Joonhyuk Ahn <96415800+dkswnsgur@users.noreply.github.com>
2024-12-12 20:32:16 +0900

    Add files via upload

---
+++ SpringBatchTutoria/src/main/java/com/example/SpringBatchTutoria/SpringBatchTutoriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchTutoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTutoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTutoriaApplication.class, args);
+	}
+
+}
---
+++ SpringBatchTutoria/src/test/java/com/example/SpringBatchTutoria/SpringBatchTutoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchTutoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTutoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
