ebbf6689e9778f39294710240c5bf270a2f08d4d
Joonhyuk Ahn <96415800+dkswnsgur@users.noreply.github.com>
2025-06-02 20:27:40 +0900

    Add files via upload

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -1,56 +1,86 @@
 <template>
-<h1>영화 정보</h1>
-<div v-for="(movie, i) in data" :key="i" class="item">
-  <figure>
-    <img :src="'${movie.imgUrl}'" :alt="movie.title">
-  </figure>
-  <div class="info">
-    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
-   <p>개봉 : {{ movie.year }}</p>
-   <p>장르 : {{ movie.category }}</p>
-   <button @:click="increseLike(i)">좋아요</button> 
-   <span>{{ movie.like }}</span>
-  </div>
-</div>
+  <Navbar />
+  <Event :text="text[eventTextNum]" />
+   {{ eventTextNum }}
+  <SearchBar :data="data_temp" @searchMovie="searchMovie($event)" />
+  <p>
+    <button @click="showAllMovie">전체보기</button>
+  </p>
+  <Movies :data="data_temp"
+   @openModal="isModal=true; selectedMovie=$event"
+   @increseLike="increseLike($event)"
+  />
+
+<Modal :data="data" :isModal="isModal" :selectedMovie="selectedMovie" @closeModal="isModal=false" /> 
 </template>
 
 <script>
+import data from './assets/movies'; //영화 데이터
+import Navbar from './components/Navbar.vue';
+import Modal from './components/Modal.vue';
+import Event from './components/Event.vue';
+import SearchBar from './components/SearchBar.vue';
+import Movies from './components/Movies.vue';
+
+console.log(data);  
+
  export default {
   name: 'APP',
   data() {
     return {
-      data: [
-      {
-        title:"노랑",
-        year:2023,
-        category:"액션, 드라마",
-        textRed:"color: red",
-        like: 0, 
-        imgUrl:'./assets/다운로드.jpg'
-      },
-      {
-        title: "아쿠아맨과 로스트 킹덤",
-        year: 2023,
-        category: "액션, 판타지, 어드밴처",
-        like: 0, 
-        imgUrl:'./assets/다운로드(1).jpg'
-      },
-      {
-        title: "3일의 휴가",
-        year: 2023,
-        category: "판타지, 드라마",
-        like: 0, 
-        imgUrl:'./assets/다운로드(2).jpg'
-      },
-      ]  
- }
- },
+      isModal: false,
+      data: data,
+      data_temp: [...data], //사본
+      selectedMovie: 0,
+      text: ['NETPLX 강렬한 운명의 드라마, 경기 크리쳐!!!','디즈니 100주년 기념작','그날 대한민국의 운명이결정됬다'],
+      eventTextNum: 0,
+      interval: null,
+    }
+  },
  methods: {
-   increseLike(i) {
-    this.data[i].like += 1;
+   increseLike(id) {
+    // this.data[i].like += 1;
+    this.data.find(movie => {
+      if(movie.id == id) {
+        movie.like += 1;
+      }
+    })
+   },
+   searchMovie(title) {
+    //영화제목이 포함된 데이터를 가져옴
+    this.data_temp = this.data.filter(movie => {
+      return movie.title.includes(title);
+    })
+   },
+   showAllMovie() {
+    this.data_temp = [...this.data];
    }
- }
- }
+
+ },
+ components: {
+  Navbar: Navbar,
+  Event: Event,
+  Modal: Modal,
+  Movies: Movies,
+  SearchBar: SearchBar,
+ },
+  mounted() {
+    console.log('mounted');
+    this.interval = setInterval(() => {
+      if (this.eventTextNum === this.text.length - 1) {
+        this.eventTextNum = 0;
+      } else {
+        this.eventTextNum += 1;
+      }
+    }, 3000);
+  },
+
+  unmounted() {
+    clearInterval(this.interval);
+  }
+}
+
+  
 </script>
 
 <style>
@@ -75,6 +105,7 @@ p {
 
 button {
   margin-right: 10px;
+  margin-top: 1rem;
 }
 
 .item {
@@ -96,5 +127,24 @@ button {
 
 .item .info {
   width: 100%;
+}
+
+.modal {
+  background : rgba(0, 0, 0, 0,7);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner {
+  background-color: #ffff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
 }
 </style>
---
+++ movie-info/src/assets/movies.js
@@ -0,0 +1,31 @@
+
+const data = [
+      {
+        id: 0,
+        title:"반도",
+        year:2023,
+        category:"액션, 드라마",
+        textRed:"color: red",
+        like: 0, 
+        imgUrl:'/assets/다운로드.jpg'
+      },
+      {
+        id: 1,
+        title: "더 포스트",
+        year: 2023,
+        category: "액션, 판타지, 어드밴처",
+        like: 0, 
+        imgUrl:'/assets/다운로드 (1).jpg'
+      },
+      {
+        id: 2,
+        title: "군도",
+        year: 2023,
+        category: "판타지, 드라마",
+        like: 0, 
+        imgUrl:'/assets/다운로드 (2).jpg'
+      },
+      ]  
+      
+      export default data;
+ 
